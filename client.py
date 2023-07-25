26c3af483246a30d9a851a12eb2169897808d7ed
Mehmet Ali Soylu <mhmtsoylu1928@gmail.com>
2023-07-25 15:12:44 +0300

    Improve client window, create socket & threading

--- client.py
+++ client.py
@@ -6,7 +6,7 @@ from threading import Thread
 
 window = Tk()
 window.title("Chat Room Application")
-window.config(bg="blue")
+window.config(bg="light blue")
 
 message_frame = Frame(window, height=100, width=100, bg="green")
 message_frame.pack()
@@ -20,5 +20,26 @@ scroll_bar.pack(side=RIGHT, fill=Y)
 msg_list.pack(side=LEFT, fill=BOTH)
 msg_list.pack()
 
+label = Label(window, text="Enter your message", fg="light blue", font="Arial", bg="blue")
+label.pack()
+
+entry_field = Entry(window, textvariable=my_msg, fg="white", width=800)
+entry_field.pack()
+
+send_button = Button(window, text="Send", font="Arial", fg="white", command=send)
+send_button.pack()
+
+quit_button = Button(window, text="Quit", font="Arial", fg="white", command=on_closing)
+quit_button.pack()
+
+
+host = "127.0.0.1"
+port = 8080
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((host, port))
+
+receive_thread = Thread(target=receive)
+receive_thread.start()
 
 mainloop()
