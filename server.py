c92e6bc83737803a3edd1322b6271c59051abbf7
Mehmet Ali Soylu <mhmtsoylu1928@gmail.com>
2023-07-25 14:41:24 +0300

    Create accept & handle client functions

--- server.py
+++ server.py
@@ -9,12 +9,34 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 sock.bind((host, port))
 
+def handle_clients(conn, address):
+    name = conn.recv(1024).decode()
+    welcome = f"Welcome {name}. You can type #quit if you want to leave the Chat Room"
+    conn.recv(bytes(welcome, "utf8"))
+    msg = f"{name} has recently joined the Chat Room"
+    broadcast(bytes(msg, "utf8"))
+    clients[conn] = name
+
+    while True:
+        msg = conn.recv(1024)
+        if msg != bytes("#quit", "utf8"):
+            broadcast(msg, name + ":")
+        else:
+            conn.send(bytes("#quit", "utf8"))
+            conn.close()
+            del clients[conn]
+            broadcast(bytes(f"{name} has left the chat room"))
+
+
+
 def accept_client_connections():
     while True:
         client_conn, client_address = sock.accept()
         print(client_address, " has connected...")
         client_conn.send(" Welcome to the Chat Room. Please type your name to continue".encode("utf8"))
         addresses[client_conn] = client_address
+
+        Thread(target=handle_clients, args=(client_conn, client_address)).start()
 
 if __name__ == "__main__":
     sock.listen(5)
