89c042ed979493f4d9ddbe8b257fc02ed8489344
Mehmet Ali Soylu <mhmtsoylu1928@gmail.com>
2023-07-25 14:21:56 +0300

    Create client, create server print message

--- server.py
+++ server.py
@@ -8,6 +8,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((host, port))
 
 sock.listen(1)
+print("The server is running and listening to client request")
 conn, address = sock.accept()
 
 message = "There is a message for you"
