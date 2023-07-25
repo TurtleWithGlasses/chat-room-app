fd5d56db22f06096d0e2dea45d1c57fbf3e3ddb3
Mehmet Ali Soylu <mhmtsoylu1928@gmail.com>
2023-07-25 14:16:02 +0300

    Server built

---
+++ server.py
@@ -0,0 +1,16 @@
+import socket
+
+host = "localhost"
+port = 8080
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+sock.bind((host, port))
+
+sock.listen(1)
+conn, address = sock.accept()
+
+message = "There is a message for you"
+conn.send(message.encode())
+
+conn.close()
