16ea00c0a81f889ba12f75d4ec32f124a21ef76e
Mehmet Ali Soylu <mhmtsoylu1928@gmail.com>
2023-07-25 14:31:27 +0300

    Improved server for bigger users, build thread

--- server.py
+++ server.py
@@ -1,17 +1,25 @@
 import socket
+from threading import Thread
 
-host = "localhost"
+host = "127.0.0.1"
 port = 8080
-
+clients = {}
+addresses = {}
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 sock.bind((host, port))
 
-sock.listen(1)
-print("The server is running and listening to client request")
-conn, address = sock.accept()
+def accept_client_connections():
+    while True:
+        client_conn, client_address = sock.accept()
+        print(client_address, " has connected...")
+        client_conn.send(" Welcome to the Chat Room. Please type your name to continue".encode("utf8"))
+        addresses[client_conn] = client_address
 
-message = "There is a message for you"
-conn.send(message.encode())
+if __name__ == "__main__":
+    sock.listen(5)
+    print("The server is running")
 
-conn.close()
+    t1 = Thread(target=accept_client_connections)
+    t1.start()
+    t1.join()
